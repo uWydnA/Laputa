00a9dde972a354652c970d15f69ff27345164a73
Make-I-promise <3487799890@qq.com>
2020-03-04 17:22:29 +0800

    yyf-video页面-初次提交

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,5 +1,66 @@
 <template>
   <div>
-    Video
+      <video-player  class="video-player vjs-custom-skin"
+          ref="videoPlayer"
+          :playsinline="true"
+          :options="playerOptions"
+      ></video-player>
   </div>
 </template>
+
+<script>
+import Vue from 'vue'
+import VideoPlayer from 'vue-video-player'
+import 'vue-video-player/node_modules/video.js/dist/video-js.css'
+import 'vue-video-player/src/custom-theme.css'
+Vue.use(VideoPlayer)
+
+export default {
+  data () {
+    return {
+      playerOptions: {
+        playbackRates: [], // 播放速度
+        autoplay: false, // 如果true,浏览器准备好时开始回放。
+        muted: false, // 默认情况下将会消除任何音频。
+        loop: false, // 导致视频一结束就重新开始。
+        preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+        language: 'zh-CN',
+        aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+        sources: [{
+          type: '', // 这里的种类支持很多种：基本视频格式、直播、流媒体等，具体可以参看git网址项目
+          src: 'https://cn-videos.dji.net/video_trans/3f8d78dc204a4c3bb7776ce04ccca2cd/1080.mp4' // url地址
+        }],
+        poster: '../../static/images/test.jpg', // 你的封面地址
+        // width: document.documentElement.clientWidth, //播放器宽度
+        notSupportedMessage: '此视频暂无法播放，请稍后再试', // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
+        controlBar: {
+          timeDivider: true,
+          durationDisplay: true,
+          remainingTimeDisplay: false,
+          fullscreenToggle: true, // 全屏按钮
+          seekToLive: false,
+          pictureInPictureToggle: false // 画中画
+        }
+      }
+    }
+  },
+  mounted () {
+    console.log(this.$refs.videoPlayer.player)
+    // this.$refs.videoPlayer.player.src('https://cn-videos.dji.net/video_trans/3f8d78dc204a4c3bb7776ce04ccca2cd/1080.mp4')
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+    .video-js .vjs-icon-placeholder {
+        width: 1em;
+        height: 100%;
+    }
+    .vjs-seek-to-live-text{
+      display:none;
+    }
+</style>
