53da49693ca80cf31e5035fc80e13cc5fb4008a5
Make-I-promise <3487799890@qq.com>
2020-03-05 08:51:16 +0800

    Merge branch 'cooperation' of https://github.com/uWydnA/Laputa

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,66 +1,179 @@
 <template>
-  <div>
-      <video-player  class="video-player vjs-custom-skin"
-          ref="videoPlayer"
-          :playsinline="true"
-          :options="playerOptions"
-      ></video-player>
-  </div>
+    <div>
+        <div class='header'>
+            <div class='user'>
+                <img :src='userImg'/>
+                <div>
+                    <h3>{{ userName }}</h3>
+                    <p v-if='location'>
+                       <img src='https://i.52112.com/icon/jpg/256/20200113/71287/3038442.jpg'/>
+                       <span>{{ location }}</span>
+                    </p>
+                </div>
+            </div>
+            <div class='follow' :class='isFollow ? "gray":""' @click='handelFollow'>
+                {{ isFollow?'已关注':'+关注'}}
+            </div>
+        </div>
+        <myvideo :videoUrl='videoUrl' :posterUrl='posterUrl' v-if='videoUrl'></myvideo>
+        <div class='share'>
+            <div class='thumb'>
+                <div class='left'>
+                    <p>
+                       <img @click='handelPraise'
+                       :src='ispraise?"https://i.52112.com/icon/jpg/256/20190827/55875/2492795.jpg":
+                       "https://i.52112.com/icon/jpg/256/20190827/55874/2492789.jpg"'/>
+                       <span>{{ praiseNum }}</span>
+                    </p>
+                    <p>
+                       <img src='https://i.52112.com/icon/jpg/256/20191127/67812/2918605.jpg'/>
+                       <span>0</span>
+                    </p>
+                    <p>
+                       <img src='https://i.52112.com/icon/jpg/256/20180926/22083/1056842.jpg'/>
+                       <span>分享</span>
+                    </p>
+                </div>
+                <div class='right'>
+                    <img @click='handelCollect'
+                    :src='isCollect?"https://i.52112.com/icon/jpg/256/20140810/393/35883.jpg":
+                    "https://i.52112.com/icon/jpg/256/20170915/8716/494850.jpg"'/>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import VideoPlayer from 'vue-video-player'
-import 'vue-video-player/node_modules/video.js/dist/video-js.css'
-import 'vue-video-player/src/custom-theme.css'
-Vue.use(VideoPlayer)
+import myvideo from '@/components/Video'
+import axios from 'axios'
+import { mapState } from 'Vuex'
 
 export default {
   data () {
     return {
-      playerOptions: {
-        playbackRates: [], // 播放速度
-        autoplay: false, // 如果true,浏览器准备好时开始回放。
-        muted: false, // 默认情况下将会消除任何音频。
-        loop: false, // 导致视频一结束就重新开始。
-        preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-        language: 'zh-CN',
-        aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
-        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-        sources: [{
-          type: '', // 这里的种类支持很多种：基本视频格式、直播、流媒体等，具体可以参看git网址项目
-          src: 'https://cn-videos.dji.net/video_trans/3f8d78dc204a4c3bb7776ce04ccca2cd/1080.mp4' // url地址
-        }],
-        poster: '../../static/images/test.jpg', // 你的封面地址
-        // width: document.documentElement.clientWidth, //播放器宽度
-        notSupportedMessage: '此视频暂无法播放，请稍后再试', // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
-        controlBar: {
-          timeDivider: true,
-          durationDisplay: true,
-          remainingTimeDisplay: false,
-          fullscreenToggle: true, // 全屏按钮
-          seekToLive: false,
-          pictureInPictureToggle: false // 画中画
-        }
-      }
+      isFollow: false,
+      ispraise: false,
+      isCollect: false,
+      praiseNum: 0,
+      videoUrl: '',
+      posterUrl: '',
+      userImg: '',
+      userName: '',
+      location: ''
     }
   },
+  components: {
+    myvideo
+  },
   mounted () {
-    console.log(this.$refs.videoPlayer.player)
-    // this.$refs.videoPlayer.player.src('https://cn-videos.dji.net/video_trans/3f8d78dc204a4c3bb7776ce04ccca2cd/1080.mp4')
+    // 请求数据
+    axios.get(`/api/v2/videos/${this.videoId}?lang=zh-Hans&platform=web&device=mobile`)
+      .then(res => {
+        // this.videoUrl = res.data.data.item.cdn_url.medium
+        // this.posterUrl = res.data.data.item.image.medium
+
+        this.userImg = res.data.data.item.user.avatar.small
+        this.userName = res.data.data.item.user.name
+        this.location = res.data.data.item.location ? res.data.data.item.location.label : ''
+        this.praiseNum = res.data.data.item.like_count
+      })
+    // console.log(this.videoId)
+    // console.log(this.$route.params)
+  },
+  computed: {
+    ...mapState('video', ['videoId'])
   },
   methods: {
-
+    // 是否关注
+    handelFollow () {
+      this.isFollow = !this.isFollow
+    },
+    // 点赞
+    handelPraise () {
+      this.ispraise = !this.ispraise
+      // 点赞数计算
+      this.ispraise ? this.praiseNum++ : this.praiseNum--
+    },
+    // 收藏
+    handelCollect () {
+      this.isCollect = !this.isCollect
+    }
   }
 }
 </script>
 
-<style scoped>
-    .video-js .vjs-icon-placeholder {
-        width: 1em;
-        height: 100%;
+<style lang="scss" scoped>
+    p,h3{
+        margin:0;
     }
-    .vjs-seek-to-live-text{
-      display:none;
+    .header{
+        display: flex;
+        padding: .5rem 2rem;
+        justify-content: space-between;
+        .user{
+            display: flex;
+            justify-content: space-between;
+            img{
+                width: 2.5rem;
+                height: 2.5rem;
+                border-radius: 50%;
+            }
+            div{
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                margin: 0 1rem;
+                h3{
+                    font-size: 1rem;
+                }
+                p{
+                    color:gray;
+                    img{
+                        width: .7rem;
+                        height: .7rem;
+                        margin-bottom: .1.7rem;
+                    }
+                }
+            }
+        }
+        .follow{
+            width: 2.6rem;
+            height: 2rem;
+            text-align: center;
+            align-self: center;
+            background: rgb(236, 235, 235);
+            font: .8rem/2rem '';
+            padding: 0 .5rem;
+            color: blue;
+        }
+        .gray{
+            color: gray;
+        }
+    }
+    .share{
+        text-align: center;
+        padding: 0 2rem;
+        img{
+            width: 1rem;
+            align-self: center;
+        }
+        .thumb{
+            padding: 1rem 0;
+            border-bottom: .1rem solid rgb(236, 236, 236);
+            display: flex;
+            justify-content: space-between;
+            .left{
+                display: flex;
+                p{
+                  display: flex;
+                  justify-content: space-between;
+                  span{
+                      align-self: center;
+                      width: 3rem;
+                  }
+                }
+            }
+        }
     }
 </style>
